1c1e3fb8fc4c6fceeac7d962550735ccf72fdc2f
Paulo Henrique <ph.sistemas.ti@gmail.com>
2020-05-12 10:02:15 -0400

    Adiconado primeiro formulário (Cadastro de Produtos)

---
+++ src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -0,0 +1,32 @@
+package br.com.casadocodigo.loja.models;
+
+public class Produto {
+	
+	private String titulo;
+	private String descricao;
+	private Integer paginas;
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Integer getPaginas() {
+		return paginas;
+	}
+	public void setPaginas(Integer paginas) {
+		this.paginas = paginas;
+	}
+	@Override
+	public String toString() {
+		return "Produto [titulo=" + titulo + ", descricao=" + descricao + ", paginas=" + paginas + "]";
+	}
+	
+	
+}
---
+++ src/main/webapp/WEB-INF/views/produtos/form.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="/casadocodigo/produtos" method="POST">
+		<div>
+			<label>Título</label>
+			<input type="text" name="titulo">
+		</div>
+		<div>
+			<label>Descrição</label>
+			<textarea rows="10" cols="20" name="descricao"></textarea>
+		</div>
+		<div>
+			<label>Número de Páginas</label>
+			<input type="text" name="paginas">
+		</div>
+		<button type="submit">Cadastrar</button>
+	</form>
+</body>
+</html>
